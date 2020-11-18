935176998292abee48eb73999bfb37afb9fcf366
rishabh246 <rishabh246@gmail.com>
2020-11-18 14:21:58 +0000

    Last touches before merge

--- include/klee/ExecutionState.h
+++ include/klee/ExecutionState.h
@@ -276,15 +276,16 @@ public:
 
   /// @brief List of Instructions executed so far
   std::vector<llvm::Instruction *> callPathInstr;
-  
+
   /// @brief Call Stack with unmangled Function call names. This
   /// is recorded only for the portions of the call path when we are inside
   /// call-trace-instr-startfn (see main.cpp in tools/klee)
   std::vector<std::string> traceCallStack;
 
   /// @brief Hash Map between CallStack and and llvm Instruction
-  std::vector<std::pair<std::vector<std::string>, llvm::Instruction *>> stackInstrMap;
-  
+  // TODO: Change to std::map
+  std::vector<std::pair<std::vector<std::string>, llvm::Instruction *>>
+      stackInstrMap;
 
   /// Statistics and information
 
@@ -366,7 +367,7 @@ public:
   // The numbers of times this state has run through Executor::stepInstruction
   std::uint64_t steppedInstructions;
 
-  std::map<std::string,std::map<int,ref<Expr>>> reused_symbols;
+  std::map<std::string, std::map<int, ref<Expr>>> reused_symbols;
 
 private:
   ExecutionState() : ptreeNode(0) {}
